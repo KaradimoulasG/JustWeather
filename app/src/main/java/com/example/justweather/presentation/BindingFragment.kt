8446931d0360af2356eae4015287321eca542dca
George <karadimoulas@gmail.com>
2023-07-25 09:32:52 +0300

    Created some abstract files and will move on with the UI and the first testing

---
+++ app/src/main/java/com/example/justweather/presentation/BindingFragment.kt
@@ -0,0 +1,33 @@
+package com.example.justweather.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BindingFragment<out T : ViewBinding> : Fragment() {
+
+    private var _binding: ViewBinding? = null
+
+    @Suppress("UNCHECKED_CAST")
+    protected val binding: T
+        get() = _binding as T
+
+    protected abstract val bindingInflater: (LayoutInflater) -> ViewBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = bindingInflater(inflater)
+        return _binding?.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/justweather/presentation/MainActivity.kt
+++ app/src/main/java/com/example/justweather/presentation/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.justweather.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.justweather.R
+import com.example.justweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
