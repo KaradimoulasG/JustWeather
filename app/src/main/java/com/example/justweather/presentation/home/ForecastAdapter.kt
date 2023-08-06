1d82f389ee73f8e7473a0cb1f525235441e44d41
George <karadimoulas@gmail.com>
2023-08-06 15:05:30 +0300

    Created a new adapter for the forecast with a new adapter item, changed a few things here and there and created a bug inside the RV of the home fragment. Will work on it another time. Onwards!

--- app/src/main/java/com/example/justweather/presentation/home/ForecastAdapter.kt
+++ app/src/main/java/com/example/justweather/presentation/home/ForecastAdapter.kt
@@ -15,34 +15,33 @@ import com.example.justweather.common.extensions.transformToDateIndented
 import com.example.justweather.domain.model.ForecastInfo
 import kotlin.math.roundToInt
 
-class ForecastAdapter : ListAdapter<ForecastInfo, ForecastAdapter.ForecastViewHolder>(DiffCallBack()) {
+class ForecastAdapter : ListAdapter<ForecastInfo, ForecastAdapter.ForecastViewHolder>(
+    DiffCallBack()
+) {
 
     private class DiffCallBack : DiffUtil.ItemCallback<ForecastInfo>() {
         override fun areItemsTheSame(oldItem: ForecastInfo, newItem: ForecastInfo) = oldItem.timestamp == newItem.timestamp
         override fun areContentsTheSame(oldItem: ForecastInfo, newItem: ForecastInfo) = oldItem == newItem
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
-        ForecastViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.forecast_adapter_item, parent, false))
-
-    override fun onBindViewHolder(holder: ForecastViewHolder, position: Int) {
-        val result = currentList[position]
-        val context = holder.itemView.context
-        val dateTimeFormatted = result.timestamp.transformToDateIndented()
-        val iconToShow = result.weatherDetails[0].icon.showWeatherIcon()
-
-        holder.dateTime.text = context.getString(R.string.date_time_title, result.timestampText)
-        Glide.with(context).load(iconToShow).into(holder.weatherIcon)
-        holder.temperature.text = context.getString(R.string.local_temp, result.mainDetails.temp.roundToInt())
-    }
-
-    fun addAll(items: List<ForecastInfo>) {
-        submitList(items)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ForecastViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.forecast_adapter_item, parent, false))
+    override fun onBindViewHolder(holder: ForecastAdapter.ForecastViewHolder, position: Int) = holder.bind(holder, position)
+    fun addAll(items: List<ForecastInfo>) = submitList(items)
 
     inner class ForecastViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var dateTime: TextView = itemView.findViewById(R.id.date_time_tv)
-        var weatherIcon: ImageView = itemView.findViewById(R.id.weather_iv)
-        var temperature: TextView = itemView.findViewById(R.id.temperature_tv)
+        private var dateTime: TextView = itemView.findViewById(R.id.date_time_tv)
+        private var weatherIcon: ImageView = itemView.findViewById(R.id.weather_iv)
+        private var temperature: TextView = itemView.findViewById(R.id.temperature_tv)
+
+        fun bind(holder: ForecastViewHolder, pos: Int) {
+            val result = currentList[pos]
+            val context = holder.itemView.context
+            val dateTimeFormatted = result.timestamp.transformToDateIndented()
+            val iconToShow = result.weatherDetails[0].icon.showWeatherIcon()
+
+            holder.dateTime.text = dateTimeFormatted
+            Glide.with(context).load(iconToShow).into(holder.weatherIcon)
+            holder.temperature.text = context.getString(R.string.local_temp, result.mainDetails.temp.roundToInt())
+        }
     }
 }
