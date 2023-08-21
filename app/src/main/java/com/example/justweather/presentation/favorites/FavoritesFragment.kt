a95ee0d36909dabb110815cd57824e5f82dc1ed7
George <karadimoulas@gmail.com>
2023-08-21 23:26:56 +0300

    Setting up last fragment and will work on it this week so the UI can be fixed

--- app/src/main/java/com/example/justweather/presentation/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/justweather/presentation/favorites/FavoritesFragment.kt
@@ -3,18 +3,54 @@ package com.example.justweather.presentation.favorites
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewbinding.ViewBinding
 import com.example.justweather.databinding.FragmentFavoritesBinding
 import com.example.justweather.presentation.BindingFragment
+import com.example.justweather.presentation.WeatherViewModel
+import com.example.justweather.presentation.WeatherViewModelEvent
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class FavoritesFragment : BindingFragment<FragmentFavoritesBinding>() {
 
     override val bindingInflater: (LayoutInflater) -> ViewBinding
         get() = FragmentFavoritesBinding::inflate
 
+    private val viewModel: WeatherViewModel by sharedViewModel()
+    private val favoritesAdapter = FavoritesAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpUi()
+        setUpViewModel()
+    }
+
+    private fun setUpUi() {
+        viewModel.getSavedCities()
+
+        binding.favoritesRv.apply {
+            layoutManager = LinearLayoutManager(
+                activity?.applicationContext,
+                LinearLayoutManager.VERTICAL,
+                false,
+            )
+            adapter = favoritesAdapter
+        }
+    }
+
+    private fun setUpViewModel() {
+        lifecycleScope.launch {
+            viewModel.state.onEach {
+                when (it.eventName) {
+                    WeatherViewModelEvent.None -> {}
+                    else -> {}
+                }
+            }.launchIn(this)
+
+        }
     }
 }
