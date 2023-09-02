94522434e457cf162c9394b21cc8469c0944feea
George <karadimoulas@gmail.com>
2023-09-02 23:40:59 +0300

    Added a unit test. Will implement a mock api response to make sure it works properly

--- app/src/test/java/com/example/justweather/ExampleUnitTest.kt
+++ app/src/test/java/com/example/justweather/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package com.example.justweather
 
-import org.junit.Test
-
+import com.example.core_domain.domain.data.OpenWeatherApi
+import com.example.core_domain.domain.data.persistence.CityDao
+import com.example.core_domain.domain.repoImpl.CityRepoImpl
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +19,35 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    private var weatherService: CityRepoImpl? = null
+    private val weatherApi: OpenWeatherApi? = null
+    private val cityDao: CityDao? = null
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        weatherService = CityRepoImpl(weatherApi!!, cityDao!!)
+    }
+
+    @Test
+    suspend fun testGetWeatherForCity_Success() {
+        // Mock the behavior of the WeatherApi when fetching weather for "Thessaloniki"
+        `when`(weatherApi?.getCityInfo("Thessaloniki")).thenReturn(null)
+        val result = weatherService?.getCityInfo("Thessaloniki")
+
+        // Verify that the result matches the expected value
+        assertEquals(null, result)
+    }
+
+    @Test
+    suspend fun testGetWeatherForCity_Error() {
+        // Mock the behavior of the WeatherApi when encountering an error
+        `when`(weatherApi?.getCityInfo("Invalid City")).thenThrow(RuntimeException("API Error"))
+        val result = weatherService?.getCityInfo("Invalid City")
+
+        // Verify that the result indicates an error
+        assertEquals("Error: Unable to fetch weather data", result)
     }
 }
--- core-domain-data/src/main/java/com/example/core_domain/domain/data/persistence/CityDao.kt
+++ core-domain-data/src/main/java/com/example/core_domain/domain/data/persistence/CityDao.kt
@@ -9,16 +9,16 @@ import com.example.core_domain.domain.model.CityInfo
 interface CityDao {
 
     @Upsert
-    suspend fun insertFavouriteCity(city: com.example.core_domain.domain.model.CityInfo)
+    suspend fun insertFavouriteCity(city: CityInfo)
 
     @Upsert
-    suspend fun savedTemporaryCity(city: com.example.core_domain.domain.model.CityInfo)
+    suspend fun savedTemporaryCity(city: CityInfo)
 
     @Query("SELECT * FROM CityInfo WHERE NULLIF(cityName, '') is NULL ORDER BY timestamp DESC LIMIT 1")
-    suspend fun getFavouriteCity(): com.example.core_domain.domain.model.CityInfo
+    suspend fun getFavouriteCity(): CityInfo
 
     @Query("SELECT * FROM CityInfo")
-    suspend fun getAllSavedCities(): List<com.example.core_domain.domain.model.CityInfo>
+    suspend fun getAllSavedCities(): List<CityInfo>
 
     @Query("SELECT EXISTS (SELECT 1 FROM CityInfo WHERE cityName = :name)")
     suspend fun isCitySaved(name: String): Boolean
