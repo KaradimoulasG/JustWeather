38554749e84603f72d5f833e91b566d925a4f5f2
George <karadimoulas@gmail.com>
2023-09-03 20:17:54 +0300

    Added a mock response for the day

--- app/src/test/java/com/example/justweather/ExampleUnitTest.kt
+++ app/src/test/java/com/example/justweather/ExampleUnitTest.kt
@@ -1,8 +1,15 @@
 package com.example.justweather
 
 import com.example.core_domain.domain.data.OpenWeatherApi
+import com.example.core_domain.domain.data.dto.cityInfo.CloudsDto
+import com.example.core_domain.domain.data.dto.cityInfo.CoordinationDto
+import com.example.core_domain.domain.data.dto.cityInfo.MainDto
+import com.example.core_domain.domain.data.dto.cityInfo.SysDto
+import com.example.core_domain.domain.data.dto.cityInfo.WeatherDto
+import com.example.core_domain.domain.data.dto.cityInfo.WindDto
 import com.example.core_domain.domain.data.persistence.CityDao
 import com.example.core_domain.domain.repoImpl.CityRepoImpl
+import com.squareup.moshi.Json
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -24,6 +31,7 @@ class ExampleUnitTest {
     private var weatherService: CityRepoImpl? = null
     private val weatherApi: OpenWeatherApi? = null
     private val cityDao: CityDao? = null
+    val mockResponse = WeatherResponse(null, null, null, null, null, null, null, "Thessaloniki",)
 
     @Before
     fun setUp() {
@@ -51,3 +59,18 @@ class ExampleUnitTest {
         assertEquals("Error: Unable to fetch weather data", result)
     }
 }
+
+data class WeatherResponse(
+    val base: String? = null,
+    val cloudsDto: CloudsDto? = null,
+    val cod: Int? = null,
+    val coord: CoordinationDto? = null,
+    val dt: Int? = null,
+    val id: Int? = null,
+    val main: MainDto? = null,
+    val name: String,
+    val sys: SysDto? = null,
+    val visibility: Int? = null,
+    val weather: List<WeatherDto>? = null,
+    val wind: WindDto? = null,
+)
