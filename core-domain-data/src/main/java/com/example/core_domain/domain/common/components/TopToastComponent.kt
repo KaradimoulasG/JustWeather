90c4625e6622ccf7d0f0261a8fad1a3cc1cd1c2c
George <karadimoulas@gmail.com>
2023-09-06 23:37:55 +0300

    Some compose stuff for today

--- core-domain-data/src/main/java/com/example/core_domain/domain/common/components/TopToastComponent.kt
+++ core-domain-data/src/main/java/com/example/core_domain/domain/common/components/TopToastComponent.kt
@@ -6,11 +6,14 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,12 +25,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 
 @Composable
-fun SlideAnimationDemo(hideAfterAnimation: () -> Unit) {
+fun SlideAnimationDemo() {
     var isVisible by remember { mutableStateOf(true) }
 
     Column(
@@ -39,14 +44,15 @@ fun SlideAnimationDemo(hideAfterAnimation: () -> Unit) {
             SlideUpBox(
                 text = "Hello, this is a sliding message box!",
                 color = Color.Blue,
+                onDismiss = { isVisible = false },
             )
 
-            // Automatically slide back up after 2 seconds
-            LaunchedEffect(isVisible) {
-                delay(2000L)
-                isVisible = false
-                hideAfterAnimation
-            }
+//             Automatically slide back up after 2 seconds
+//            LaunchedEffect(isVisible) {
+//                delay(2000L)
+//                isVisible = false
+//                hideAfterAnimation
+//            }
         }
     }
 }
@@ -55,6 +61,7 @@ fun SlideAnimationDemo(hideAfterAnimation: () -> Unit) {
 fun SlideUpBox(
     text: String,
     color: Color,
+    onDismiss: () -> Unit,
 ) {
     val slideOffset by animateDpAsState(
         targetValue = 100.dp,
@@ -71,12 +78,30 @@ fun SlideUpBox(
             .padding(16.dp)
             .animateContentSize(),
         verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Text(
             text = text,
             color = MaterialTheme.colorScheme.onSurface,
             fontSize = 16.sp,
             lineHeight = 24.sp,
+            textAlign = TextAlign.Center,
         )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+//                isVisible = false
+                onDismiss()
+            },
+            modifier = Modifier.align(Alignment.End),
+        ) {
+            Text(text = "Dismiss")
+        }
     }
+}
+
+@Preview
+@Composable
+fun previewBox() {
+    SlideAnimationDemo()
 }
